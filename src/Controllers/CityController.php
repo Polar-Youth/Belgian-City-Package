74d95a218f58ec7a048c317d1e34ae13803b4fc4
Tjoosten <Tjoosten3@gmail.com>
2017-03-28 02:03:37 +0200

    Update

--- src/Controllers/CityController.php
+++ src/Controllers/CityController.php
@@ -3,6 +3,7 @@
 namespace PolarYouth\Cities\Controller;
 
 use App\Http\Controllers\Controller;
+use PolarYouth\Cities\Models\Cities;
 
 /**
  * Class CityController
@@ -12,10 +13,49 @@ use App\Http\Controllers\Controller;
 class CityController extends Controller
 {
     /**
-     * CityController constructor.
+     * @var Cities
      */
-    public function __construct()
+    private $city;
+
+    /**
+     * CityController constructor.
+     * @param Cities $city
+     */
+    public function __construct(Cities $city)
     {
-        //
+        $this->city = $city;
+    }
+
+    /**
+     * Find a city by his postal code.
+     *
+     * @param  int $name
+     * @return mixed
+     */
+    public function findByCode($name)
+    {
+        return $this->city->where('postal_code', $name)->get();
+    }
+
+    /**
+     * Find a city by his name.
+     *
+     * @param  string $name
+     * @return mixed
+     */
+    public function findByName($name)
+    {
+        return $this->city->where('name', $name)->get();
+    }
+
+    /**
+     * Get all the cities.
+     *
+     * @return \Illuminate\Database\Eloquent\Builder|static
+     */
+    public function allCities()
+    {
+        $query = $this->city->with('province');
+        return $query->get();
     }
 }
