2ceb45828214792661ff04e717805fb1817839ed
Tim Joosten <Tjoosten3@gmail.com>
2017-03-28 21:54:57 +0200

    Update gulp

---
+++ docs/Gulpfile.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('sass', function () {
+    return gulp.src('./assets/sass/*.scss')
+        .pipe(sass.sync().on('error', sass.logError))
+        .pipe(gulp.dest('./assets/css'));
+});
+
+gulp.task('sass:watch', function () {
+    gulp.watch('./assets/sass/*.scss', ['sass']);
+});
+
+gulp.task('copy-fonts', function() {
+    gulp.src('./node_modules/font-awesome/fonts/**/*.{ttf,woff,eof,svg,woff2}')
+        .pipe(gulp.dest('./assets/fonts'));
+});
+
+gulp.task('copy-js', function() {
+    gulp.src('./node_modules/')
+});
